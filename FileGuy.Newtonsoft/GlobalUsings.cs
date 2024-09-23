e54295f7a3d6ffcdef001ea1b71015ce3cccfe2e
Mathieu Moreault <seranx@gmail.com>
2024-09-23 14:22:30 -0400

    Updated nugets and removed obsoletes

--- FileGuy.Newtonsoft/GlobalUsings.cs
+++ FileGuy.Newtonsoft/GlobalUsings.cs
@@ -3,6 +3,7 @@
 global using System.Reflection;
 global using Microsoft.Extensions.DependencyInjection;
 global using Newtonsoft.Json;
+global using ToolBX.AutoConfig;
 global using ToolBX.AutoInject;
 global using ToolBX.NetAbstractions.IO.Streaming;
 global using ToolBX.Reflection4Humans.Extensions;
--- FileGuy/GlobalUsings.cs
+++ FileGuy/GlobalUsings.cs
@@ -1,11 +1,13 @@
 // Global using directives
 
+global using System.Collections.Immutable;
 global using System.IO.Compression;
 global using System.Reflection;
 global using System.Text;
 global using ToolBX.FileGuy.Resources;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Options;
+global using ToolBX.AutoConfig;
 global using ToolBX.AutoInject;
 global using ToolBX.DescriptiveEnums;
 global using ToolBX.NetAbstractions.IO;
